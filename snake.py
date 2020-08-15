a01d3c00dbde457ad47da210af4146cdeb3e1ef5
alexanderajju <alexanderajju@gmail.com>
2020-08-15 18:04:46 +0530

    Modified version

--- snake.py
+++ snake.py
@@ -1,48 +1,76 @@
-import random
 import curses
-s= curses.initscr()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
-w.keypad(1)
-w.timeout(100)
-snk_x = int(sw/4)
-snk_y = int(sh/2)
-snake = [
-    [snk_y, snk_x],
-    [snk_y, int(snk_x-1)],
-    [snk_y, int(snk_x-2)]
-]
+from random import randint
 
-food = [int(sh/2), int(sw/2)]
-w.addch(food[0], food[1], curses.ACS_PI)
+# setup window
+s = curses.initscr()
+sh, sw = s.getmaxyx()
+win = curses.newwin(sh, sw, 0, 0) # y,x
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1) # -1
+
+# snake and food
+snake = [(4, 10), (4, 9), (4, 8)]
+food = (10, 20)
+
+win.addch(food[0], food[1], '#')
+# game logic
+score = 0
+
+ESC = 27
 key = curses.KEY_RIGHT
-while True:
-    next_key = w.getch()
-    key = key if next_key == -1 else next_key
-    if snake[0][0] in [0, sh] or snake[0][1] in [0, sw] or snake[0] in snake[1:]:
-        curses.endwin()
-        quit()
-    new_head = [snake[0][0], snake[0][1]]
+
+while key != ESC:
+    win.addstr(0, 2, 'Score ' + str(score) + ' ')
+    win.timeout(150 - (len(snake)) // 5 + len(snake)//10 % 120) # increase speed
+
+    prev_key = key
+    event = win.getch()
+    key = event if event != -1 else prev_key
+
+    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
+        key = prev_key
+
+    # calculate the next coordinates
+    y = snake[0][0]
+    x = snake[0][1]
     if key == curses.KEY_DOWN:
-        new_head[0] += 1
+        y += 1
     if key == curses.KEY_UP:
-        new_head[0] -= 1
+        y -= 1
     if key == curses.KEY_LEFT:
-        new_head[1] -= 1
+        x -= 1
     if key == curses.KEY_RIGHT:
-        new_head[1] += 1
-    snake.insert(0, new_head)
+        x += 1
+
+    snake.insert(0, (y, x)) # append O(n)
+
+    # check if we hit the border
+    if y == 0: break
+    if y == sh: break
+    if x == 0: break
+    if x == sw: break
+
+    # if snake runs over itself
+    if snake[0] in snake[1:]: break
+
     if snake[0] == food:
-        food = None
-        while food is None:
-            nf = [
-                random.randint(1, sh - 1),
-                random.randint(1, sw - 1)
-            ]
-            food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_PI)
+        # eat the food
+        score += 1
+        food = ()
+        while food == ():
+            food = (randint(1,18), randint(1,58))
+            if food in snake:
+                food = ()
+        win.addch(food[0], food[1], '#')
     else:
-        tail = snake.pop()
-        w.addch(tail[0], tail[1], ' ')
-    w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+        # move snake
+        last = snake.pop()
+        win.addch(last[0], last[1], ' ')
+
+    win.addch(snake[0][0], snake[0][1], '*')
+
+curses.endwin()
+print(f"Final score = {score}")
